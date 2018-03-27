b7bf9d8cbc52d3a96e542db3446ed248271fb6d4
Ben Kucera <benkucera@gmail.com>
2018-03-26 21:56:57 -0400

    refactor entry, add app.ts

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,3 @@
-import { Bodies, Engine, Render, World } from "matter-js"
+import app from './app'
 
-
-
-const engine = Engine.create()
-const render = Render.create({
-	element: document.body,
-	engine,
-})
-
-const boxA = Bodies.rectangle(400, 200, 80, 80)
-const boxB = Bodies.rectangle(400, 50, 80, 80)
-const ground = Bodies.rectangle(400, 610, 810, 60, {isStatic: true})
-
-World.add(engine.world, [boxA, boxB, ground])
-
-Engine.run(engine)
-Render.run(render)
+app.start()
