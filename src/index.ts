1870cc3a58b2f2ceae8041fbbaa7e33f5d2bcca2
Ben Kucera <benkucera@gmail.com>
2018-03-26 21:52:09 -0400

    Add matter-js

---
+++ src/index.ts
@@ -0,0 +1,18 @@
+import { Bodies, Engine, Render, World } from "matter-js"
+
+
+
+const engine = Engine.create()
+const render = Render.create({
+	element: document.body,
+	engine,
+})
+
+const boxA = Bodies.rectangle(400, 200, 80, 80)
+const boxB = Bodies.rectangle(400, 50, 80, 80)
+const ground = Bodies.rectangle(400, 610, 810, 60, {isStatic: true})
+
+World.add(engine.world, [boxA, boxB, ground])
+
+Engine.run(engine)
+Render.run(render)
