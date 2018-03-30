7b24346b6737ad0967a226e1cbabf22522afbf48
Ben Kucera <benkucera@gmail.com>
2018-03-30 04:39:10 -0400

    deployment, webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -3,51 +3,55 @@ const webpack = require('webpack')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 const CleanWebpackPlugin = require('clean-webpack-plugin')
 
-const mode = 'development' // 'production'
 
-module.exports = {
-  mode,
-  entry: {
-    main: './src/index.ts',
-    // vendor: ['matter-js'],
-  },
-  module: {
-    rules: [
-      {
-        test: /\.tsx?$/,
-        use: 'ts-loader',
-        exclude: /node_modules/
-      }
-    ]
-  },
-  resolve: {
-    extensions: [ '.tsx', '.ts', '.js' ]
-  },
-  output: {
-    filename: '[name].[chunkhash].js',
-    path: path.resolve(__dirname, 'dist')
-  },
-  plugins: [
-    new CleanWebpackPlugin(),
-	  new HtmlWebpackPlugin({
-		  title: "Evac Sim"
-	  })
-  ],
-  devtool: 'inline-source-map',
-  devServer: {
-	contentBase: path.join(__dirname, "dist"),
-	compress: true,
-	port: 9000
-  },
-  // optimization: {
-	// 	splitChunks: {
-	// 		cacheGroups: {
-	// 			vendor1: {
-	// 				name: "matter-js",
-	// 				test: "matter-js",
-	// 				enforce: true
-	// 			},
-	// 		}
-	// 	}
-	// },
-};
+module.exports = env => {
+  
+  const mode = env && env.production ? 'production' : 'development'
+
+  return {
+    mode,
+    entry: {
+      main: './src/index.ts',
+      // vendor: ['matter-js'],
+    },
+    module: {
+      rules: [
+        {
+          test: /\.tsx?$/,
+          use: 'ts-loader',
+          exclude: /node_modules/
+        }
+      ]
+    },
+    resolve: {
+      extensions: ['.tsx', '.ts', '.js']
+    },
+    output: {
+      filename: '[name].[chunkhash].js',
+      path: path.resolve(__dirname, 'dist')
+    },
+    plugins: [
+      new CleanWebpackPlugin(),
+      new HtmlWebpackPlugin({
+        title: "Evac Sim"
+      })
+    ],
+    devtool: 'inline-source-map',
+    devServer: {
+      contentBase: path.join(__dirname, "dist"),
+      compress: true,
+      port: 9000
+    },
+    // optimization: {
+    // 	splitChunks: {
+    // 		cacheGroups: {
+    // 			vendor1: {
+    // 				name: "matter-js",
+    // 				test: "matter-js",
+    // 				enforce: true
+    // 			},
+    // 		}
+    // 	}
+    // },
+  }
+}
