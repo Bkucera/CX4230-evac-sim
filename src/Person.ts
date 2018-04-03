3a2f22bd3fb538cd540094aef50ab90a319e244a
Younmin Bae <ybae35@gatech.edu>
2018-04-02 21:21:25 -0400

    Hardcoded defaultExits

--- src/Person.ts
+++ src/Person.ts
@@ -1,8 +1,16 @@
 import { assign } from 'lodash'
 import { Bodies, Body, Vector } from "matter-js";
-import { personColor } from './globals';
-import { defaultExits } from './Mapper';
-import Exit from './Exit';
+import { w, h, personColor } from './globals';
+//import { defaultExits } from './Mapper';
+
+// Hardcoded defaultExits 
+const wallThickness = 10
+const roomLength = 25
+const defaultExitWidth = 25
+const defaultExits = [
+		Vector.create(2 * w - 4 * defaultExitWidth, h - wallThickness / 2),
+		Vector.create(2 * w - 4 * defaultExitWidth, wallThickness / 2)
+	]
 
 /*
 
@@ -17,14 +25,12 @@ Current behavior:
 - Walks in random direction. Has no idea where an exit is.
 
 */
-
-
 export default class Person {
 	public name: string
 	public id: number
 	public initPosition: Vector = { x: 400, y: 400 }
 	public body : Body
-	public exit: Exit
+	public exit: Vector
 	constructor(options: Partial<Person>) {
 		assign(this, options)
 
@@ -43,7 +49,7 @@ export default class Person {
 		// Choose random exit using random int generator
 		// (min is inclusive, max is exclusive)
 		let min = Math.ceil(0)
-  		let max = Math.floor(defualtExits.length)
+  		let max = Math.floor(defaultExits.length)
   		this.exit = defaultExits[Math.floor(Math.random() * (max - min)) + min]
 
 		// Start walking randomly every 90 ms, repeat forever
@@ -66,8 +72,8 @@ export default class Person {
 	}
 
 	private getExitDirectionForce() {
-		let deltaX = this.exit.position.x - this.body.position.x
-		let deltaY = this.exit.position.y - this.body.position.y
+		let deltaX = this.exit.x - this.body.position.x
+		let deltaY = this.exit.y - this.body.position.y
 		return Vector.div(Vector.normalise({
 			x: deltaX,
 			y: deltaY,
